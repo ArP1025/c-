fc2a1e6f3cc7e33d08f7add0f750e2ac6c3af204
ArP1025 <1557138035@qq.com>
2025-08-01 22:37:21 +0800

    Add files via upload

--- 未命名2.cpp
+++ 未命名2.cpp
@@ -3,7 +3,7 @@
 int main() {
     int num, digit1, digit2, digit3;
 
-    printf("100到999之间的水仙花数有：\n");
+    
     for (num = 100; num <= 999; num++) {
         digit1 = num / 100;
         digit2 = (num / 10) % 10;
